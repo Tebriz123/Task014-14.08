d4861b486c0fecc7cbc5a4de77d05751c804e491
Tebriz123 <necefovtebriz5555@gmail.com>
2025-08-14 17:06:19 +0400

    initial commit

---
+++ Task014/Task014/Models/Helper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task014.Models
+{
+    internal static class Helper
+    {
+        public static bool IsOdd(this int num)
+        {
+            if(num %2 == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool IsEven(this int num)
+        {
+            if (num % 2 != 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool HasDigit(this string name)
+        {
+            for(int i = 0; i < name.Length; i++)
+            {
+                if(Char.IsDigit(name[i]))
+                {
+                    return true;
+                }
+               
+            }
+            return false;
+        }
+        public static bool CheckPassword(this string password)
+        {
+            
+            for(int i = 0; i < password.Length; i++)
+            {
+                char upperChar = Char.ToUpper(password[i]);
+                char lowerChar = Char.ToLower(password[i]);
+                if(password.Length <= 8) 
+                {
+
+                    if (upperChar != password[i])
+                    {
+                        if (lowerChar != password[i])
+                        {
+                            if (!Char.IsDigit(password[i]))
+                            {
+                                
+                            }
+
+                        }
+                    }
+                    return false;
+
+                }
+                }
+            return true;
+        
+        }
+        public static string Capitalize(this string name)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Char.ToUpper(name[0]));
+            for(int i = 1; i < name.Length; i++)
+            {
+                sb.Append(Char.ToLower(name[i]));
+            }
+            return sb.ToString();
+            
+        }
+    }
+}
---
+++ Task014/Task014/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+using System;
+using System.Security.Cryptography;
+using Task014.Models;
+
+namespace Task014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 51;
+            int b = 52;
+            string password = "abbbbbb1";
+            string password2 = "abbbbbb";
+            string password3 = "Ali122343";
+            string password4 = "123alTAar";
+            Console.WriteLine(a.IsOdd());
+            Console.WriteLine(b.IsOdd());
+            Console.WriteLine("-----------------------");
+            Console.WriteLine(a.IsEven());
+            Console.WriteLine(b.IsEven());
+            Console.WriteLine("-------------------------");
+            Console.WriteLine(password.HasDigit());
+            Console.WriteLine(password2.HasDigit());
+            Console.WriteLine("------------------------");
+            Console.WriteLine(password3.CheckPassword());
+            Console.WriteLine(password.CheckPassword());
+            Console.WriteLine(password4.CheckPassword());
+        }
+    }
+}
